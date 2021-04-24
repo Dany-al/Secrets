b075c9035364b3d1c1503fcdf03db0bc9be215d3
Dany <danyallaw1@gmail.com>
2021-04-24 03:56:18 +0300

    Add sumbit Secret.

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ mongoose.set("useCreateIndex" ,true );
 const userSchema = new mongoose.Schema( {
     email:String,
     password:String,
-    googleId:String
+    googleId:String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -88,11 +89,37 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    }else{
-        res.redirect("/login");
-    }
+    User.find({"secret":{$ne: null}},function(err,foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets",{usersWithSecrets:foundUsers});
+        }
+    });
+});
+
+app.get("/submit",function(req,res){
+     if(req.isAuthenticated()){
+         res.render("submit");
+     }else{
+         res.redirect("/login");
+     }
+});
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret= submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets")
+                });
+            }
+        }
+    });
 });
 
 app.get("/logout",function(req,res){
@@ -130,7 +157,7 @@ req.login(user,function(err){
         });
         
     }
-})
+});
 
 });
 
